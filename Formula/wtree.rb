aade82aea978f2853de45b667d4de3ef79cb484f
lmaia-22 <luismsm14@gmail.com>
2026-09-01 23:27:59 +0100

    feat: add wtree formula v0.1.0

---
+++ Formula/wtree.rb
@@ -0,0 +1,31 @@
+class Wtree < Formula
+  desc "Bare-clone git worktrees, without the ceremony"
+  homepage "https://github.com/lmaia-22/wtree"
+  url "https://github.com/lmaia-22/wtree/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "91187802f2ff4fdc47df2560fd0383488d920c18b961e7fda687e118c541de48"
+  license "MIT"
+
+  depends_on "fzf"
+
+  def install
+    bin.install "wtree.sh" => "wtree"
+    (share/"wtree").install "shell/wtree.zsh"
+    (share/"wtree").install "shell/wtree.bash"
+  end
+
+  def caveats
+    <<~EOS
+      To enable `wtree switch` and tab-completion, source the shell
+      integration file for your shell in your rc file:
+
+        zsh:  source "#{HOMEBREW_PREFIX}/share/wtree/wtree.zsh"
+        bash: source "#{HOMEBREW_PREFIX}/share/wtree/wtree.bash"
+
+      Then restart your shell.
+    EOS
+  end
+
+  test do
+    assert_match "wtree - bare-clone git worktrees", shell_output("#{bin}/wtree --help")
+  end
+end
