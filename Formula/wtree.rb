41ba1d35834392f7daebaf6bf0a7788f7fbd841d
lmaia-22 <luismsm14@gmail.com>
2026-09-02 02:19:52 +0100

    fix: drop ANSI escapes from caveats (brew audit --strict)
    Homebrew's style audit forbids ANSI codes in caveats since they leak
    into logs, brew info --json, etc. CI caught this on the first real
    run. Caveats now render the banner in plain text.

--- Formula/wtree.rb
+++ Formula/wtree.rb
@@ -15,12 +15,12 @@ class Wtree < Formula
 
   def caveats
     <<~EOS
-         \e[38;2;0;255;136m⢀⣠⣴⣶⣶⣦⣄⡀\e[0m
-       \e[38;2;0;220;160m⢀⣴⣿⣿⣿⣿⣿⣿⣿⣦⡀\e[0m     \e[1;37mwtree\e[0m \e[90mv#{version}\e[0m
-      \e[38;2;0;185;180m⢠⣿⣿⣿\e[38;2;220;40;40m⣿\e[38;2;0;185;180m⣿⣿\e[38;2;220;40;40m⣿\e[38;2;0;185;180m⣿⣿⡄\e[0m    \e[90mbare-clone git worktrees\e[0m
-       \e[38;2;0;150;200m⠙⠻⢿⣿⣿⣿⣿⠿⠟⠋\e[0m     \e[38;2;0;185;180mwithout the ceremony\e[0m
-            \e[38;2;180;120;60m║\e[0m
-            \e[38;2;140;90;40m╩\e[0m
+         ⢀⣠⣴⣶⣶⣦⣄⡀
+       ⢀⣴⣿⣿⣿⣿⣿⣿⣿⣦⡀     wtree v#{version}
+      ⢠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄    bare-clone git worktrees
+       ⠙⠻⢿⣿⣿⣿⣿⠿⠟⠋     without the ceremony
+            ║
+            ╩
 
       To enable `wtree switch` and tab-completion, source the shell
       integration file for your shell in your rc file:
